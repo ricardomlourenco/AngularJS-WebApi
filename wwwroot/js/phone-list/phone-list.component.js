88ede19968a83b581e5e147a30a9dcf9db6f3e18
ricardomlourenco <ricardomlourenco@gmail.com>
2022-08-15 19:19:21 -0400

    adds  to load items from json file...

--- wwwroot/js/phone-list/phone-list.component.js
+++ wwwroot/js/phone-list/phone-list.component.js
@@ -1,25 +1,12 @@
 angular.module("phoneList").component("phoneList", {
   // This name is what AngularJS uses to match to the `<phone-list>` element.
   templateUrl: "js/phone-list/phone-list.component.html",
-  controller: function PhoneListController() {
-    this.orderProp = "age";
+  controller: function PhoneListController($http) {
+    var self = this;
+    self.orderProp = "age";
 
-    this.phones = [
-      {
-        name: "Nexus S",
-        snippet: "Fast just got faster with Nexus S.",
-        age: 1,
-      },
-      {
-        name: "Motorola XOOM™ with Wi-Fi",
-        snippet: "The Next, Next Generation tablet.",
-        age: 2,
-      },
-      {
-        name: "MOTOROLA XOOM™",
-        snippet: "The Next, Next Generation tablet.",
-        age: 3,
-      },
-    ];
+    $http.get("js/phones.json").then(function (response) {
+      self.phones = response.data.slice(0, 5);
+    });
   },
 });
