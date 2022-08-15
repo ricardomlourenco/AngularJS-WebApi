28d1d6bca6156a30ff017ae8d71361c46ba2b41a
ricardomlourenco <ricardomlourenco@gmail.com>
2022-08-15 19:07:09 -0400

    adds sorting feature in the phone-list component...

--- wwwroot/js/phone-list/phone-list.component.js
+++ wwwroot/js/phone-list/phone-list.component.js
@@ -2,18 +2,23 @@ angular.module("phoneList").component("phoneList", {
   // This name is what AngularJS uses to match to the `<phone-list>` element.
   templateUrl: "js/phone-list/phone-list.component.html",
   controller: function PhoneListController() {
+    this.orderProp = "age";
+
     this.phones = [
       {
         name: "Nexus S",
         snippet: "Fast just got faster with Nexus S.",
+        age: 1,
       },
       {
         name: "Motorola XOOM™ with Wi-Fi",
         snippet: "The Next, Next Generation tablet.",
+        age: 2,
       },
       {
         name: "MOTOROLA XOOM™",
         snippet: "The Next, Next Generation tablet.",
+        age: 3,
       },
     ];
   },
